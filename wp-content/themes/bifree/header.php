500a476d8e85ed60a326ff9837605b2f7b4264dc
bifree <bifree2bfree@gmail.com>
2021-07-16 00:44:22 +0900

    restart2

---
+++ wp-content/themes/bifree/header.php
@@ -0,0 +1,27 @@
+<header>
+	<div class="header">
+		<div class="logo">
+			<img src="wp-content/themes/Engress/logo.png">
+		</div>
+		<div class="top-menu">
+			<nav>
+				<ul class="top-list">
+					<li><a href="#" class="top-list-item">ホーム</a></li>
+					<li><a href="#" class="top-list-item">お知らせ</a></li>
+					<li><a href="#" class="top-list-item">ブログ</a></li>
+					<li><a href="#" class="top-list-item">コース・料金</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="top-contact">
+			<div class="phone-contact">
+				<p class="small-text">平日08:00〜20:00</p>
+				0123-456-7890
+			</div>
+			<div class="contacts">
+				<a href="" class="btn btn_leaflet">資料請求</a>
+				<a href="" class="btn btn_contact">お問い合わせ</a>
+			</div>
+		</div>
+	</div>
+</header>
